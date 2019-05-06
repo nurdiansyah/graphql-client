3167a325df634a747328ecf7d5f7d38212bd4703
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2019-05-06 17:12:52 +0700

    initial project

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: '@deboxsoft/devel-react',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/?*[-.]+(spec|test).[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node']
+};
